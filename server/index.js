e08c677835400f82bb2592eefdafad6544d5ea43
ejiwen <skibnewrim@gmail.com>
2021-07-17 12:32:20 -0700

    first commit

---
+++ server/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+// app.get('/', (req, res) => {
+//   console.log("Hello from server");
+//   res.end();
+// })
+app.use(express.static('public'));
+
+app.listen(3000, () => console.log("SERVER IS LISTENING NOW ..."));
