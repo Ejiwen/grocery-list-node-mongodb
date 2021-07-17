6d3b455d5c63250589b964a9f747570e4c671138
ejiwen <skibnewrim@gmail.com>
2021-07-17 13:16:56 -0700

    start point for a lot of project in the future inchallah

--- server/index.js
+++ server/index.js
@@ -7,4 +7,4 @@ const app = express();
 // })
 app.use(express.static('public'));
 
-app.listen(3000, () => console.log("SERVER IS LISTENING NOW ..."));
+app.listen(3000, () => console.log("SERVER IS LISTENING NOW ... !!!"));
