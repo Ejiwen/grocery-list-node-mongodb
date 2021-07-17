f64bf39a1262fefcdf454ffc82d4d64212fd1e44
ejiwen <skibnewrim@gmail.com>
2021-07-17 13:32:43 -0700

    import component ...

---
+++ client/src/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <h1> Hello from APP component </h1>
+    )
+  }
+}
+
+export default App;
